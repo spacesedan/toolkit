c0e064858e1940bc87aa8b8b98b66d0afa23ff6d
Jose Duarte <jduarte0912@gmail.com>
2022-12-07 20:10:24 -0800

    Initial entry

---
+++ tools_test.go
@@ -0,0 +1,12 @@
+package toolkit
+
+import "testing"
+
+func TestTools_RandomString(t *testing.T) {
+	var testTools Tools
+
+	s := testTools.RandomString(10)
+	if len(s) != 10 {
+		t.Errorf("wrong length random string returned; expected 10, but got %d", len(s))
+	}
+}
