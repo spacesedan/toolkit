8d9e8f491412419c44d2f94dee35cdb1a4195a44
Jose Duarte <jduarte0912@gmail.com>
2022-12-13 10:28:25 -0800

    added PushJSONtoRemote method

--- tools.go
+++ tools.go
@@ -1,6 +1,7 @@
 package toolkit
 
 import (
+	"bytes"
 	"crypto/rand"
 	"encoding/json"
 	"errors"
@@ -308,4 +309,36 @@ func (t *Tools) ErrorJSON(w http.ResponseWriter, err error, status ...int) error
 
 	return t.WriteJSON(w, statusCode, payload)
 
+}
+
+// PushJSONtoRemote posts arbitrary data to some URL as JSON and returns the response, status code, and error, if any.
+// The final parameter, client, is optional. If none is specified, we use the standard http.Client.
+func (t *Tools) PushJSONtoRemote(uri string, data interface{}, client ...*http.Client) (*http.Response, int, error) {
+	// create json
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	// check for custom client
+	httpClient := &http.Client{}
+	if len(client) > 0 {
+		httpClient = client[0]
+	}
+
+	// build a request and set the header
+	request, err := http.NewRequest(http.MethodPost, uri, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return nil, 0, err
+	}
+	request.Header.Set("Content-Type", "application/json")
+	// call the remote url
+	response, err := httpClient.Do(request)
+	if err != nil {
+		return nil, 0, err
+	}
+	defer response.Body.Close()
+
+	// send response back
+	return response, response.StatusCode, nil
 }
